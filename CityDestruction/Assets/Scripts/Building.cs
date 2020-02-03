fdd75c63211bbe0b616297e750f476beef24a398
pf-ushiyama <ushiyama@profire.co.jp>
2020-02-03 18:41:37 +0900

    初期コミット

---
+++ CityDestruction/Assets/Scripts/Building.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Building : MonoBehaviour
+{
+    [SerializeField]
+    float necessaryPower;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.relativeVelocity.magnitude > necessaryPower && transform.childCount > 0)
+        {
+            while (transform.childCount > 0)
+            {
+                var child = transform.GetChild(0);
+
+                child.SetParent(this.transform.parent);
+                child.gameObject.SetActive(true);
+                // child.transform.localScale = new Vector3(   child.transform.localScale.x*this.transform.localScale.x,
+                //                                             child.transform.localScale.y*this.transform.localScale.y,
+                //                                             child.transform.localScale.z*this.transform.localScale.z);
+                // child.transform.localPosition =  new Vector3(   child.transform.localPosition.x*this.transform.localScale.x,
+                //                                                 child.transform.localPosition.y*this.transform.localScale.y,
+                //                                                 child.transform.localPosition.z*this.transform.localScale.z);
+
+            }
+
+            GameObject.Destroy(this.gameObject);
+        }
+    }
+}
---
+++ CityDestruction/Assets/Scripts/Cannon.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannon : MonoBehaviour
+{
+    [Header("ステータス")]
+    [SerializeField]
+    float power;
+
+    [Header("参照したいプレハブ")]
+    [SerializeField]
+    GameObject shellsPrefab;
+    
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            var shellObj = GameObject.Instantiate(shellsPrefab);
+            shellObj.transform.SetParent(this.transform);
+            shellObj.transform.localPosition = Vector3.zero;
+            shellObj.GetComponent<Rigidbody>().AddForce(this.transform.forward * power);
+        }
+    }
+}
